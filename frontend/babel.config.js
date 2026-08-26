f1ad6309f70494eb68ce5ee00f2f9533293ff42d
jorgepliesa <jorgepliesa@gmail.com>
2026-08-26 02:32:00 +0200

    fixing build

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -4,7 +4,9 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
-      ['@babel/plugin-transform-private-methods', { loose: true }]
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }]
     ],
   };
 };
